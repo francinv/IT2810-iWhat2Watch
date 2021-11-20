38ec847015ebc9404140cd091ed0ab9f079dc13e
kavusi98 <kavusikan1998@gmail.com>
2021-11-20 22:27:38 +0100

    (#9) updated server links

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import {DB_URL} from "@env";
+//import {DB_URL} from "@env";
+import "dotenv/config"
 
+
+//console.log(process.env.DB_URL)
 export const apolloClient = new ApolloClient({
-  uri: DB_URL ? DB_URL : "http://localhost:4000/graphql",
+  uri: process.env.DB_URL ? process.env.DB_URL : "http://localhost:4000/graphql",
+  //uri: DB_URL ? DB_URL : "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
