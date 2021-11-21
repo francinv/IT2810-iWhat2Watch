bc4ac1dc24f12efdfdcf93a23156be7e7d9ea9e8
Dherik <dherikjd@stud.ntnu.no>
2021-11-21 16:40:59 +0100

    (#11) Removed dotenv

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -1,11 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-//import {DB_URL} from "@env";
-import "dotenv/config"
 
 
 //console.log(process.env.DB_URL)
 export const apolloClient = new ApolloClient({
-  uri: process.env.DB_URL ? process.env.DB_URL : "http://localhost:4000/graphql",
-  //uri: DB_URL ? DB_URL : "http://localhost:4000/graphql",
+  uri: "http://it2810-29.idi.ntnu.no:4000/graphql",
+  //uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
