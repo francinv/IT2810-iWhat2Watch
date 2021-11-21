8769b60d8dbddcbde752cb25cc88fe6225de4eff
Dherik <dherikjd@stud.ntnu.no>
2021-11-21 16:41:29 +0100

    (#11) Removed unused code

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
-//console.log(process.env.DB_URL)
 export const apolloClient = new ApolloClient({
   uri: "http://it2810-29.idi.ntnu.no:4000/graphql",
   //uri: "http://localhost:4000/graphql",
