c23a52319ac7759b8b50a74d24d7bc49b34e3eef
Dherik <dherikjd@stud.ntnu.no>
2021-11-22 04:47:38 +0100

    (#15) Commented utils

--- frontend/src/util/dateConverter.ts
+++ frontend/src/util/dateConverter.ts
@@ -1,12 +1,15 @@
+/* Converts date object to UNIX date format (seconds after 1970) */
 export function convertDateToUnixDate(date: Date) {
   return Math.round(date.getTime() / 1000);
 }
 
+/* Converts UNIX date format to date object */
 export function convertUnixDateToDate(unixNumber: number) {
   const date = new Date(unixNumber * 1000);
   return date;
 }
 
+/* Makes date object readable */
 export function formatDateAsString(date: Date) {
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
