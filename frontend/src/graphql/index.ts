e5ff38b3b7cc49090a00799ad24f46396770cad6
Dherik <dherikjd@stud.ntnu.no>
2021-11-22 01:20:42 +0100

    (#15) Fixed merge overwrite

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import {DB_URL} from "@env";
-//import "dotenv/config"
+
 export const apolloClient = new ApolloClient({
-  uri: process.env.DB_URL ? process.env.DB_URL : "http://localhost:4000/graphql",
+  uri: "http://it2810-29-idi.ntnu.no:4000/graphql",
   cache: new InMemoryCache(),
 });
